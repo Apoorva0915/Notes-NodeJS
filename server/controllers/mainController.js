b13cdccd5b9ace471cdb22be05141bd028bd92a3
Apoorva Kesarwani <apoorvakesarwani001@gmail.com>
2023-07-24 00:11:24 +0530

    Initial commit

---
+++ server/controllers/mainController.js
@@ -0,0 +1,23 @@
+// get homepage
+
+exports.homepage=async(req,res)=>{
+    const locals={
+        title:"NodeJs Notes",
+        description:"Free NodeJs Notes App"
+    };
+    res.render("index",{
+        locals,
+        layout:"../views/layouts/front-page"
+    })
+}
+
+
+// get about
+
+exports.about=async(req,res)=>{
+    const locals={
+        title:"About-NodeJs Notes",
+        description:"Free NodeJs Notes App"
+    };
+    res.render("about",locals)
+}
